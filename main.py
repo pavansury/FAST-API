4499b2c68c6f9ae45c7da63696bf6c36e27efbb7
pavansury <pavansuryawanshi202@gmail.com>
2025-09-24 09:35:42 +0530

    fast api basic clear

--- main.py
+++ main.py
@@ -13,4 +13,31 @@ class Tea(BaseModel):
 
 test: list[Tea] = []
 
+@app.get("/")
+def read_root():
+    return {"message": 'welcome to tea api'}
 
+@app.get("/teas")
+def teas():
+    return test
+
+@app.post("/teas")
+def add_tea(tea: Tea):
+    test.append(tea)
+    return tea
+
+@app.put("/teas/{tea_id}")
+def update_tea(tea_id: int, updated_tea: Tea):
+    for index, tea in enumerate(test):
+        if tea.id == tea_id:
+            test[index] = updated_tea
+            return updated_tea
+    return {"error": "Tea not found"}
+
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id: int):
+    for index, tea in enumerate(test):
+        if tea.id == tea_id:
+            deleted_tea = test.pop(index)
+            return deleted_tea
+    return {"error": "Tea not found"}
